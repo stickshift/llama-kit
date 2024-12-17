7fd68046f7b849e9698382b286e6650d2858b518
Andrew Young <stickshift@users.noreply.github.com>
2024-12-16 20:11:11 -0500

    Llama 3.2 11B (#4)
    * Added support for instruct and text models
    
    * Added support for 11B llama model
    
    * First attempt at bfloat16
    
    * Test refactoring
    
    * Added model tests
    
    * Added model tests
    
    * Moved device back into ModelConfig
    
    * Share attention mask across all layers

--- src/llama_kit/model.py
+++ src/llama_kit/model.py
@@ -3,13 +3,12 @@ from pathlib import Path
 from typing import Iterator, Mapping, NamedTuple, Sequence, override
 
 from llama_models.llama3.api import Tokenizer
-from llama_models.llama3.reference_impl.model import RMSNorm
 import numpy as np
 import torch
 from torch import Tensor, nn
 from torch.nn import functional as F
 
-from .tools import default_arg
+from .tools import default_arg, torch_device
 
 __all__ = [
     "LlamaAttention",
@@ -19,16 +18,19 @@ __all__ = [
     "LlamaHead",
     "LlamaLayer",
     "LlamaModel",
+    "Message",
     "ModelConfig",
     "ModelParameters",
     "Tokenizer",
-    "generate_text",
     "load_config",
     "load_parameters",
     "load_tokenizer",
+    "masked_attention_bias",
+    "render_prompt",
     "rope_frequencies",
     "rope_rotate",
     "rope_swap",
+    "unpack_parameters",
 ]
 
 
@@ -60,6 +62,10 @@ class ModelConfig(NamedTuple):
 
     rope_theta: float
 
+    device: torch.device
+
+    dtype: torch.dtype
+
 
 def load_config(checkpoint_name: str, **kwargs) -> ModelConfig:
     """Load Llama3 config from checkpoint params.json."""
@@ -89,6 +95,8 @@ def load_config(checkpoint_name: str, **kwargs) -> ModelConfig:
         "n_kv_heads": hparams["n_kv_heads"],
         "rope_theta": hparams["rope_theta"],
         "d_ffn": d_ffn,
+        "device": torch_device(),
+        "dtype": torch.bfloat16,
     }
 
     # Override with kwargs
@@ -117,48 +125,67 @@ def load_parameters(config: ModelConfig, **kwargs) -> ModelParameters:
     # Remap Meta's parameter names
     params = {}
 
+    # Inject prefix for multimodal checkpoints
+    prefix = "text_model." if "text_model.tok_embeddings.weight" in checkpoint_params else ""
+
+    # Validate dtype
+    assert checkpoint_params[f"{prefix}tok_embeddings.weight"].dtype == config.dtype
+
     # Embeddings
     params |= {
-        "model.embeddings.weight": checkpoint_params["tok_embeddings.weight"],
+        "model.embeddings.weight": checkpoint_params[f"{prefix}tok_embeddings.weight"],
     }
 
     # Layers
     for layer_id in range(config.n_layers):
         params |= {
             f"model.layers.{layer_id}.attention.normalize.weight": checkpoint_params[
-                f"layers.{layer_id}.attention_norm.weight"
+                f"{prefix}layers.{layer_id}.attention_norm.weight"
             ],
             f"model.layers.{layer_id}.attention.w_queries.weight": checkpoint_params[
-                f"layers.{layer_id}.attention.wq.weight"
+                f"{prefix}layers.{layer_id}.attention.wq.weight"
             ],
             f"model.layers.{layer_id}.attention.w_keys.weight": checkpoint_params[
-                f"layers.{layer_id}.attention.wk.weight"
+                f"{prefix}layers.{layer_id}.attention.wk.weight"
             ],
             f"model.layers.{layer_id}.attention.w_values.weight": checkpoint_params[
-                f"layers.{layer_id}.attention.wv.weight"
+                f"{prefix}layers.{layer_id}.attention.wv.weight"
             ],
             f"model.layers.{layer_id}.attention.w_output.weight": checkpoint_params[
-                f"layers.{layer_id}.attention.wo.weight"
+                f"{prefix}layers.{layer_id}.attention.wo.weight"
             ],
-            f"model.layers.{layer_id}.ffn.normalize.weight": checkpoint_params[f"layers.{layer_id}.ffn_norm.weight"],
+            f"model.layers.{layer_id}.ffn.normalize.weight": checkpoint_params[
+                f"{prefix}layers.{layer_id}.ffn_norm.weight"
+            ],
             f"model.layers.{layer_id}.ffn.w_input.weight": checkpoint_params[
-                f"layers.{layer_id}.feed_forward.w3.weight"
+                f"{prefix}layers.{layer_id}.feed_forward.w3.weight"
             ],
             f"model.layers.{layer_id}.ffn.w_gate.weight": checkpoint_params[
-                f"layers.{layer_id}.feed_forward.w1.weight"
+                f"{prefix}layers.{layer_id}.feed_forward.w1.weight"
             ],
             f"model.layers.{layer_id}.ffn.w_output.weight": checkpoint_params[
-                f"layers.{layer_id}.feed_forward.w2.weight"
+                f"{prefix}layers.{layer_id}.feed_forward.w2.weight"
             ],
         }
 
     # Head
     params |= {
-        "head.normalize.weight": checkpoint_params["norm.weight"],
-        "head.w_output.weight": checkpoint_params["output.weight"],
+        "head.normalize.weight": checkpoint_params[f"{prefix}norm.weight"],
+        "head.w_output.weight": checkpoint_params[f"{prefix}output.weight"],
     }
 
     return params
+
+
+def unpack_parameters(params: ModelParameters, path: Sequence[str]) -> ModelParameters:
+    """Extract component parameters."""
+    # Calculate component prefix, e.g. model.layers.0.norm
+    prefix = ".".join(path) + "."
+
+    # Extract relevant keys and strip prefix
+    component_params = {k[len(prefix) :]: v for k, v in params.items() if k.startswith(prefix)}
+
+    return component_params
 
 
 # ------------------------------------------------------------------------------
@@ -180,11 +207,12 @@ def load_tokenizer(config: ModelConfig) -> Tokenizer:
 class LlamaEmbeddings(nn.Embedding):
     """Llama token embeddings layer."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__(
             num_embeddings=config.vocab_size,
             embedding_dim=config.d_model,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
 
@@ -193,14 +221,16 @@ class LlamaEmbeddings(nn.Embedding):
 # ------------------------------------------------------------------------------
 
 
-def rope_frequencies(config: ModelConfig, device: torch.device, n: int):
+def rope_frequencies(config: ModelConfig, n: int):
     """Compute RoPE cos and sin rotation matrices."""
     # Hyperparameters
     base = config.rope_theta
     d = config.d_head
+    device = config.device
+    dtype = config.dtype
 
     # Calculate thetas
-    i = torch.arange(d // 2, device=device)
+    i = torch.arange(d // 2, dtype=dtype).to(device)
     thetas = base ** (-2 * i / d)
 
     # Duplicate each theta, e.g. [theta_0, theta_1] -> [theta_0, theta_0, theta_1, theta_1]
@@ -240,26 +270,73 @@ def rope_rotate(x, r_cos, r_sin):
     return (x * r_cos) + (rope_swap(x) * r_sin)
 
 
+def masked_attention_bias(config: ModelConfig, n: int) -> Tensor:
+    """Compute reusable masked attention bias term M.
+
+    Returns:
+        Tensor: (n, n) diagonal matrix w/ upper triangular elements set to -inf, 0 otherwise.
+    """
+    from torch import logical_not, ones, tril, zeros  # noqa: PLC0415
+
+    # Parameters
+    device = config.device
+    dtype = config.dtype
+
+    # Initialize m with zeros
+    m = zeros(n, n, device=device, dtype=dtype)
+
+    # Create boolean mask w/ main diagonal and below set to False
+    mask = logical_not(tril(ones(n, n, device=device, dtype=torch.bool)))
+
+    # Fill upper triangular region to -inf
+    m = m.masked_fill_(mask, float("-inf"))
+
+    return m
+
+
+class LlamaNorm(torch.nn.Module):
+    """Normalizes embeddings using RMSNorm.
+
+    See RMSNorm in llama-models.
+    """
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        self.eps = config.rms_norm_eps
+        self.weight = nn.Parameter(
+            torch.ones(
+                config.d_model,
+                device=config.device,
+                dtype=config.dtype,
+            )
+        )
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        return self._norm(x) * self.weight
+
+
 class LlamaAttention(nn.Module):
     """Llama attention layer."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__()
 
         self.config = config
 
         # Input normalization
-        self.normalize = RMSNorm(
-            config.d_model,
-            config.rms_norm_eps,
-        ).to(device)
+        self.normalize = LlamaNorm(config)
 
         # Queries projection
         self.w_queries = nn.Linear(
             in_features=config.d_model,
             out_features=config.n_heads * config.d_head,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
         # Keys projection
@@ -267,7 +344,8 @@ class LlamaAttention(nn.Module):
             in_features=config.d_model,
             out_features=config.n_kv_heads * config.d_head,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
         # Values projection
@@ -275,7 +353,8 @@ class LlamaAttention(nn.Module):
             in_features=config.d_model,
             out_features=config.n_kv_heads * config.d_head,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
         # Output projection
@@ -283,14 +362,12 @@ class LlamaAttention(nn.Module):
             in_features=config.d_model,
             out_features=config.d_model,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
     @override
-    def forward(self, x: Tensor, r_cos: Tensor, r_sin: Tensor) -> Tensor:
-        # Match input device
-        device = x.device
-
+    def forward(self, x: Tensor, r_cos: Tensor, r_sin: Tensor, mask: Tensor) -> Tensor:
         # Save residuals
         residual = x
 
@@ -316,13 +393,8 @@ class LlamaAttention(nn.Module):
         q = rope_rotate(q, r_cos, r_sin)
         k = rope_rotate(k, r_cos, r_sin)
 
-        # Compute masked attention bias M
-        n = len(x)
-        mask = torch.ones(n, n, dtype=torch.bool, device=device).tril(diagonal=0)
-        m = torch.zeros(n, n, device=device).masked_fill_(mask.logical_not(), float("-inf"))
-
         # Compute attention for all heads in parallel
-        scores = q @ k.transpose(-2, -1) / np.sqrt(self.config.d_head) + m
+        scores = q @ k.transpose(-2, -1) / np.sqrt(self.config.d_head) + mask
         a = F.softmax(scores, dim=-1) @ v
 
         # Combine attention heads
@@ -353,21 +425,19 @@ class LlamaAttention(nn.Module):
 class LlamaFFN(nn.Module):
     """Llama feed-forward network."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__()
 
         # Input normalization
-        self.normalize = RMSNorm(
-            config.d_model,
-            config.rms_norm_eps,
-        ).to(device)
+        self.normalize = LlamaNorm(config)
 
         # Input projection
         self.w_input = nn.Linear(
             in_features=config.d_model,
             out_features=config.d_ffn,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
         # Gate projection
@@ -375,7 +445,8 @@ class LlamaFFN(nn.Module):
             in_features=config.d_model,
             out_features=config.d_ffn,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
         # Output projection
@@ -383,7 +454,8 @@ class LlamaFFN(nn.Module):
             in_features=config.d_ffn,
             out_features=config.d_model,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
     @override
@@ -414,17 +486,17 @@ class LlamaFFN(nn.Module):
 class LlamaLayer(nn.Module):
     """Llama transformer layer."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__()
 
-        self.attention = LlamaAttention(config, device)
+        self.attention = LlamaAttention(config)
 
-        self.ffn = LlamaFFN(config, device)
+        self.ffn = LlamaFFN(config)
 
     @override
-    def forward(self, x: Tensor, r_cos: Tensor, r_sin: Tensor) -> Tensor:
+    def forward(self, x: Tensor, r_cos: Tensor, r_sin: Tensor, mask: Tensor) -> Tensor:
         # Attention
-        x = self.attention(x, r_cos, r_sin)
+        x = self.attention(x, r_cos, r_sin, mask)
 
         # FFN
         x = self.ffn(x)
@@ -440,29 +512,37 @@ class LlamaLayer(nn.Module):
 class LlamaModel(nn.Module):
     """Combines embeddings and layers in reusable module."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__()
 
         self.config = config
 
-        self.embeddings = LlamaEmbeddings(config, device)
+        self.embeddings = LlamaEmbeddings(config)
 
-        self.layers = nn.ModuleList(LlamaLayer(config, device) for _ in range(config.n_layers))
+        self.layers = nn.ModuleList(LlamaLayer(config) for _ in range(config.n_layers))
 
     @override
     def forward(self, token_ids: Tensor) -> Tensor:
-        # Match input device
-        device = token_ids.device
+        # Precompute values shared across layers:
+        #
+        #   * RoPE rotation matrices
+        #   * Masked attention bias
+        #
 
-        # Compute cos and sin rotation matrices once for entire sequence
-        r_cos, r_sin = rope_frequencies(self.config, device, len(token_ids))
+        n = len(token_ids)
+
+        # RoPE rotation matrices
+        r_cos, r_sin = rope_frequencies(self.config, n)
+
+        # Masked attention bias
+        mask = masked_attention_bias(self.config, n)
 
         # Map tokens to embeddings
         x = self.embeddings(token_ids)
 
         # Transform token embeddings to semantic embeddings
         for layer in self.layers:
-            x = layer(x, r_cos, r_sin)
+            x = layer(x, r_cos, r_sin, mask)
 
         return x
 
@@ -475,21 +555,19 @@ class LlamaModel(nn.Module):
 class LlamaHead(nn.Module):
     """Llama prediction head."""
 
-    def __init__(self, config: ModelConfig, device: torch.device):
+    def __init__(self, config: ModelConfig):
         super().__init__()
 
         # Input normalization
-        self.normalize = RMSNorm(
-            config.d_model,
-            config.rms_norm_eps,
-        ).to(device)
+        self.normalize = LlamaNorm(config)
 
         # Output projection
         self.w_output = nn.Linear(
             in_features=config.d_model,
             out_features=config.vocab_size,
             bias=False,
-            device=device,
+            device=config.device,
+            dtype=config.dtype,
         )
 
     @override
@@ -512,12 +590,11 @@ class LlamaCausalLMHead(LlamaHead):
     def __init__(
         self,
         config: ModelConfig,
-        device: torch.device,
         temperature: float | None = None,
         top_k: int | None = None,
         top_p: float | None = None,
     ):
-        super().__init__(config, device)
+        super().__init__(config)
 
         self.temperature = default_arg(temperature, 0.6)
         self.top_k = default_arg(top_k, 50)
@@ -581,14 +658,40 @@ class LlamaCausalLMHead(LlamaHead):
 # ------------------------------------------------------------------------------
 
 
+class Message(NamedTuple):
+    """Structured message for instruct model."""
+
+    role: str
+
+    content: str
+
+
+def render_prompt(messages: Sequence[Message]) -> str:
+    """Render messages with special instruct tokens."""
+    prompt = ""
+
+    for data in messages:
+        message = data
+
+        # Convert dicts to Messages
+        if isinstance(message, dict):
+            message = Message(**message)
+
+        prompt += f"<|start_header_id|>{message.role}<|end_header_id|>\n\n"
+        prompt += message.content
+        prompt += "<|eot_id|>"
+
+    prompt += "<|start_header_id|>assistant<|end_header_id|>\n\n"
+
+    return prompt
+
+
 class LlamaGenerator(nn.Module):
     """Llama text generator."""
 
     def __init__(
         self,
         config: ModelConfig,
-        device: torch.device,
-        stop_tokens: Sequence[int] | None = None,
         temperature: float | None = None,
         top_k: int | None = None,
         top_p: float | None = None,
@@ -596,40 +699,42 @@ class LlamaGenerator(nn.Module):
     ):
         super().__init__()
 
-        self.device = device
+        self.config = config
 
-        self.stop_tokens = default_arg(stop_tokens, ())
+        self.tokenizer = load_tokenizer(config)
 
         self.max_tokens = default_arg(max_tokens, 32)
 
-        self.model = LlamaModel(config, device)
+        self.model = LlamaModel(config)
 
         self.head = LlamaCausalLMHead(
             config,
-            device,
             temperature=temperature,
             top_k=top_k,
             top_p=top_p,
         )
 
-    def __call__(self, token_ids: Sequence[int], **kwargs) -> Iterator[int]:
+    def __call__(self, prompt: str | Sequence[Message], **kwargs) -> Iterator[str]:
         """Generate token ids until stop token or we exceed max tokens."""
         # Prepare model
         self.model.eval()
         self.head.eval()
 
-        # Make mutable copy of token ids
-        token_ids = list(token_ids)
+        # Encode messages
+        if not isinstance(prompt, str):
+            prompt = render_prompt(prompt)
+
+        # Tokenize
+        token_ids = self.tokenizer.encode(prompt, bos=True, eos=False, allowed_special="all")
 
         # Override fields with kwargs
         max_tokens = kwargs.get("max_tokens", self.max_tokens)
-        stop_tokens = kwargs.get("stop_tokens", self.stop_tokens)
 
         with torch.no_grad():
             # Generate output until we get a stop token or we exceed max_tokens.
             for _ in range(max_tokens):
                 # Load token ids into a tensor
-                x = torch.tensor(token_ids, device=self.device)
+                x = torch.tensor(token_ids, device=self.config.device, dtype=torch.int64)
 
                 # Transform token_ids into semantic embeddings
                 x = self.model(x)
@@ -638,34 +743,11 @@ class LlamaGenerator(nn.Module):
                 token_id = self.head(x)
 
                 # Check stopping criteria
-                if token_id in stop_tokens:
+                if token_id in self.tokenizer.stop_tokens:
                     break
 
                 # Yield token
-                yield token_id
+                yield self.tokenizer.decode([token_id])
 
                 # Append to end of sequence
                 token_ids.append(token_id)
-
-
-# ------------------------------------------------------------------------------
-# Text Generation Pipeline
-# ------------------------------------------------------------------------------
-
-
-def generate_text(
-    tokenizer: Tokenizer,
-    generator: LlamaGenerator,
-    prompt: str,
-    **kwargs,
-) -> Iterator[str]:
-    """Generate text one token at a time."""
-    # Split prompt into tokens
-    token_ids = tokenizer.encode(prompt, bos=True, eos=False)
-
-    # Generate new token ids
-    for token_id in generator(token_ids, **kwargs):
-        # Decode token id
-        token = tokenizer.decode([token_id])
-
-        yield token
--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -1,6 +1,6 @@
 import torch
 
-from llama_kit.model import LlamaGenerator, load_config, load_parameters, load_tokenizer
+from llama_kit.model import LlamaGenerator, load_config, load_parameters
 
 
 def test_load_state_dict(device: torch.device):
@@ -9,13 +9,13 @@ def test_load_state_dict(device: torch.device):
     #
 
     # I loaded config for Llama 3.2 3B checkpoint
-    config = load_config("Llama3.2-3B")
+    config = load_config("Llama3.2-3B", device=device)
 
     # I loaded parameters from checkpoint
     params = load_parameters(config, map_location=device)
 
     # I created a generator
-    generator = LlamaGenerator(config, device=device)
+    generator = LlamaGenerator(config)
 
     #
     # Whens
@@ -32,19 +32,16 @@ def test_load_state_dict(device: torch.device):
     assert torch.equal(generator.model.embeddings.get_parameter("weight"), params["model.embeddings.weight"])
 
 
-def test_generate(device: torch.device):
+def test_323b_text(device: torch.device):
     #
     # Givens
     #
 
     # I loaded config for Llama 3.2 3B checkpoint
-    config = load_config("Llama3.2-3B")
-
-    # I created a tokenizer
-    tokenizer = load_tokenizer(config)
+    config = load_config("Llama3.2-3B", device=device)
 
     # I created a generator w/ token sampling disabled
-    generator = LlamaGenerator(config, device, stop_tokens=tokenizer.stop_tokens, temperature=0)
+    generator = LlamaGenerator(config, temperature=0)
 
     # I loaded state from checkpoint
     generator.load_state_dict(load_parameters(config, map_location=device))
@@ -56,15 +53,157 @@ def test_generate(device: torch.device):
     # Whens
     #
 
-    # I split prompt into tokens
-    token_ids = tokenizer.encode(prompt, bos=True, eos=False)
-
     # I generate next token
-    token_id = next(generator(token_ids))
+    token = next(generator(prompt))
 
     #
     # Thens
     #
 
     # token should be "delta"
-    assert tokenizer.decode([token_id]).strip() == "delta"
+    assert token.strip() == "delta"
+
+
+def test_3211b_text(device: torch.device):
+    #
+    # Givens
+    #
+
+    # I loaded config for Llama 3.2 11B checkpoint
+    config = load_config("Llama3.2-11B-Vision", device=device)
+
+    # I created a generator w/ token sampling disabled
+    generator = LlamaGenerator(config, temperature=0)
+
+    # I loaded state from checkpoint
+    generator.load_state_dict(load_parameters(config, map_location=device))
+
+    # Greek prompt
+    prompt = "alpha beta gamma"
+
+    #
+    # Whens
+    #
+
+    # I generate next token
+    token = next(generator(prompt))
+
+    #
+    # Thens
+    #
+
+    # token should be "delta"
+    assert token.strip() == "delta"
+
+
+def test_323b_instruct(device: torch.device):
+    #
+    # Givens
+    #
+
+    # I loaded config for Llama 3.2 3B Instruct checkpoint
+    config = load_config("Llama3.2-3B-Instruct", device=device)
+
+    # I created a generator w/ token sampling disabled
+    generator = LlamaGenerator(config, temperature=0)
+
+    # I loaded state from checkpoint
+    generator.load_state_dict(load_parameters(config, map_location=device))
+
+    # Boston prompt
+    prompt = [
+        {
+            "role": "user",
+            "content": "What is the capital of Massachusetts? Answer in one word.",
+        }
+    ]
+
+    #
+    # Whens
+    #
+
+    # I generate next token
+    token = next(generator(prompt))
+
+    #
+    # Thens
+    #
+
+    # token should be "Boston"
+    assert token.strip() == "Boston"
+
+
+def test_3211b_instruct(device: torch.device):
+    #
+    # Givens
+    #
+
+    # I loaded config for Llama 3.2 11B Instruct checkpoint
+    config = load_config("Llama3.2-11B-Vision-Instruct", device=device)
+
+    # I created a generator w/ token sampling disabled
+    generator = LlamaGenerator(config, temperature=0)
+
+    # I loaded state from checkpoint
+    generator.load_state_dict(load_parameters(config, map_location=device))
+
+    # Boston prompt
+    prompt = [
+        {
+            "role": "user",
+            "content": "What is the capital of Massachusetts? Answer in one word.",
+        }
+    ]
+
+    #
+    # Whens
+    #
+
+    # I generate next token
+    token = next(generator(prompt))
+
+    #
+    # Thens
+    #
+
+    # token should be "Boston"
+    assert token.strip() == "Boston"
+
+
+def test_max_tokens(device: torch.device):
+    #
+    # Givens
+    #
+
+    # I loaded config for Llama 3.2 3B Instruct checkpoint
+    config = load_config("Llama3.2-3B-Instruct", device=device)
+
+    # I created a generator w/ max tokens of 10
+    max_tokens = 10
+    generator = LlamaGenerator(config, max_tokens=max_tokens)
+    generator.load_state_dict(load_parameters(config, map_location=device))
+
+    # I create an open ended prompt
+    prompt = [
+        {
+            "role": "user",
+            "content": "Tell me a story about dragons.",
+        }
+    ]
+
+    #
+    # Whens
+    #
+
+    # I generate response
+    tokens = []
+    for token in generator(prompt):
+        assert len(tokens) < max_tokens
+        tokens.append(token)
+
+    #
+    # Thens
+    #
+
+    # token should have max_tokens elements
+    assert len(tokens) == max_tokens
